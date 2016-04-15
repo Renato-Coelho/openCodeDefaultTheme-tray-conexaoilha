68ca23c3cebc800871e17082ba30295b7a470957
Renato Abreu <self.renatoabreu@gmail.com>
2016-04-15 16:01:42 -0300

    Transition on categoriesMenu is working, but the submenu does not hide when touching outside it

--- js/base.js
+++ js/base.js
@@ -3,10 +3,12 @@ var input = document.querySelector(".input");
 input.addEventListener("focus", function () {
 	var search = this.parentNode.parentNode.parentNode;
 	search.classList.add("searchHighlighting");
+	console.log('*** searchHighlighting');
 });
 
 input.addEventListener("blur", function () {
 	var search = this.parentNode.parentNode.parentNode;
 	search.classList.remove("searchHighlighting");
+	console.log("searchHighlightingOFF");
 })
 
